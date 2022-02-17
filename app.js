0117b30647468009862ba07db510189c7c7cb900
gsomer <gasomer75@hotmail.com>
2022-02-17 00:48:39 -0800

    add html template literal to generate html file

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
 
-    console.log('================');
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
