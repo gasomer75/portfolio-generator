f2f7ccba739acb0c66e716d7ffa10ed89cfd13e1
gsomer <gasomer75@hotmail.com>
2022-02-17 12:51:23 -0800

    add user prompt

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
+//     console.log('Portfolio complete! Check out index.html to see the output!')
+// });
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
