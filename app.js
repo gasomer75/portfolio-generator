898760b3157eca8590ab5ff3b80576fe66e45d0c
gsomer <gasomer75@hotmail.com>
2022-02-20 01:00:01 -0800

    add projects and separate by featured/nonfeatured

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
 
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!')
-// });
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -139,10 +133,17 @@ Add a New Project
     });
 };
 
+
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('index.html', pageHTML, err => {
+            if (err) throw err;
+
+            console.log('Portfolio complete! Check out index.html to see the output!')
+        });
     });
     
     
